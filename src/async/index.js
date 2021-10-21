0f422a1d6e373d2d2c09257e31d147f9351ad40a
Edwar Aguiar <Aguiar.Edwar@gmail.com>
2021-10-21 07:55:12 -0500

    El eltimo Lab Demo - Resolviendo Problemas con Async y Await - Fin del Curso

---
+++ src/async/index.js
@@ -0,0 +1,34 @@
+//Async y Await
+
+const doSomethingAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(() => resolve('Do Something Async'), 3000)
+        : reject(new Error('Test Error'))
+    });
+}
+
+const doSomething = async () => {
+    const something = await doSomethingAsync();
+    console.log(something);
+}
+
+console.log('Before');
+doSomething();
+console.log('After');
+
+
+// Como capturar los errores
+
+const anotherFunction = async () => {
+    try {
+        const something = await doSomethingAsync();
+        console.log(something);
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+console.log('Before 1');
+doSomething();
+console.log('After 1');
